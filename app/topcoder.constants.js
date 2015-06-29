8705e98808b4708862d5ad0a0b24cba9ddcad2a9
Nick Litwin <nlitwin@appirio.com>
2015-06-28 22:13:11 -0700

    Add temporary hardcoded constants

---
+++ app/topcoder.constants.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  app.module('topcoder').constant('CONSTANTS', {
+    name: 'development',
+    API_URL: 'https://api.topcoder-dev.com/v3',
+    API_URL_V2: 'https://api.topcoder-dev.com/v2',
+    clientId: 'JFDo7HMkf0q2CkVFHojy3zHWafziprhT',
+    domain: 'topcoder-dev.com',
+    auth0Domain: 'topcoder-dev.auth0.com',
+    auth0Callback: 'https://www.topcoder-dev.com/reg2/callback.action'
+  });
+})();
