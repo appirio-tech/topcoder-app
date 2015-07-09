d7f94eb267429040546ab35de974e0c9d5ca4b92
vikasrohit <vikasrohit4@yahoo.com>
2015-07-09 19:34:36 +0530

    SUP-896, Merge dashboard code into topcoder-app
    Removed ng-include and used nested states.

--- app/member-dashboard/member-dashboard.routes.js
+++ app/member-dashboard/member-dashboard.routes.js
@@ -14,20 +14,62 @@
       dashboard: {
         abstract: true,
         parent: 'root',
-        template: '<div ui-view></div>',
+        templateUrl: 'member-dashboard/member-dashboard.html',
+        controller: 'dashboard as db',
         data: {
           authRequired: true,
         }
       },
-      landing: {
+      'dashboard.landing': {
         url: '/my-dashboard',
-        templateUrl: 'member-dashboard/member-dashboard.html',
-        controller: 'dashboard as db',
+        //controller: 'dashboard as db',
         authenticate: true,
         parent: 'dashboard',
         data: {
           authRequired: true,
           title: 'Dashboard'
+        },
+        views: {
+          'my-challenges-widget': {
+            templateUrl: "member-dashboard/my-challenges/my-challenges.html",
+            controller: 'MyChallengesCtrl',
+            controllerAs: 'vm'
+          },
+          'srms-widget' :{
+            templateUrl : 'member-dashboard/upcoming-srms/upcaming-srms.html',
+            controller: 'UpcomingSRMsCtrl',
+            controllerAs: 'vm'
+          },
+          'member-program-widget' : {
+            templateUrl : 'member-dashboard/member-program/member-program.html',
+            controller: 'MemberProgramCtrl',
+            controllerAs: 'vm'
+          },
+          'helpful-links-widget' : {
+            templateUrl : 'member-dashboard/helpful-links/helpful-links.html',
+            controller: 'HelpfulLinksCtrl',
+            controllerAs: 'vm'
+          },
+          'subscribe-updates-widget' : {
+            templateUrl : 'member-dashboard/subscribe-updates/subscribe-updates.html',
+            controller: 'SubscribeUpdatesCtrl',
+            controllerAs: 'vm'
+          },
+          'blog-post-widget' : {
+            templateUrl : 'member-dashboard/blog-post/blog-feed.html',
+            controller: 'BlogPostCtrl',
+            controllerAs: 'vm'
+          },
+          'profile-header-widget' : {
+            templateUrl : 'member-dashboard/member-profile/welcome-back.html',
+            controller: 'WelcomeBackCtrl',
+            controllerAs: 'vm'
+          },
+          'marketing-message-widget' : {
+            templateUrl : 'member-dashboard/marketing-message/marketing-message.html',
+            controller: 'MarketingMessageCtrl',
+            controllerAs: 'vm'
+          }
         }
       }
     };
