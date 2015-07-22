dcffa674fd0033b5dc518e94789629c46e64bd50
Thabo <tha.bo.fletcher@gmail.com>
2015-07-22 16:49:48 -0700

    notifications poc

--- app/topcoder.controller.js
+++ app/topcoder.controller.js
@@ -3,9 +3,20 @@
 
   angular.module('topcoder').controller('TopcoderController', TopcoderController);
 
-  TopcoderController.$inject = [];
+  TopcoderController.$inject = ['notifications', 'NotificationService'];
 
-  function TopcoderController() {
-
+  function TopcoderController(notifications, NotificationService) {
+    var vm = this;
+		NotificationService.getNotifications().then(function(data) {
+      vm.notifications = data.result.content;
+      
+      angular.forEach(vm.notifications, function(notification) {
+        if (notification.severity === "HIGH") {
+          notifications.showError({message: 'You received a notification of type: ' + notification.notificationTypeId });
+        } else {
+          notifications.showWarning({message: 'You received a notification of type: ' + notification.notificationTypeId });
+        }
+      })
+    });
   };
 })();
