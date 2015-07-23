0ed4a7c1c9f3eb34bf88957eeffcd4c09706a147
Nick Litwin <nlitwin@appirio.com>
2015-07-23 14:03:48 -0700

    Remove old comments and move mock data to tests folder

--- app/member-dashboard/ios-program/ios-program.controller.js
+++ app/member-dashboard/ios-program/ios-program.controller.js
@@ -23,14 +23,15 @@
 
       MemberCertService.getMemberRegistration(userId, CONSTANTS.SWIFT_PROGRAM_ID)
       .then(function(res) {
-        console.log('registered or not: ', res.plain());
         if (res !== undefined) {
           ChallengeService.getiOSChallenges()
           .then(function(challenges) {
+            // When filtering by reviewType is fixed on the backend,
+            // we can show both types of challenges. For now, it's showing internal
+            // challenges
+
             // var peerChallenges = challenges[0];
             // var iOSChallenges = challenges[1];
-            // console.log(peerChallenges);
-            // console.log('ios ', iOSChallenges);
             // vm.challenges = [peerChallenges[0], iOSChallenges[0], iOSChallenges[1]];
             vm.challenges = challenges;
             console.log(vm.challenges);
@@ -40,34 +41,6 @@
             vm.registered = true;
             $log.debug(err);
           });
-
-          // V2
-          // ChallengeService.getChallenges({review: 'PEER'})
-          // .then(function(response) {
-          //   var peerChallenges = response.data.data.slice(0, 1);
-          //   var numPeerChallenges = peerChallenges.length;
-
-          //   ChallengeService.getChallenges({review: 'COMMUNITY,INTERNAL'})
-          //   .then(function(response) {
-          //     var nonPeerChallenges = response.data.data.slice(0, 3);
-
-          //     if (numPeerChallenges === 1) {
-          //       vm.challenges = peerChallenges.concat(nonPeerChallenges.slice(0, 2));
-
-          //     } else if (numPeerChallenges === 0) {
-          //       vm.challenges = nonPeerChallenges;
-          //     }
-
-          //     Helpers.formatArray(vm.challenges);
-
-          //     for (var i = 0; i < vm.challenges.length; i++) {
-          //       var challenge = vm.challenges[i];
-          //       Helpers.processChallenge(challenge);
-          //     }
-
-          //     vm.registered = true;
-          //   });
-          // });
         }
       })
       .catch(function(err) {
