38b3ecc93744ea953f3c942354dbada0c6151216
Nick Litwin <nlitwin@appirio.com>
2015-08-10 14:42:52 -0700

    Add new relic to index file

--- app/topcoder.module.js
+++ app/topcoder.module.js
@@ -27,12 +27,14 @@
     .module('topcoder', dependencies)
     .run(appRun);
 
-  appRun.$inject = ['$rootScope', '$state', 'TcAuthService', '$cookies', 'Helpers', '$log', 'NotificationService'];
+  appRun.$inject = ['$rootScope', '$state', 'TcAuthService', '$cookies', 'Helpers', '$log', 'NotificationService', '$window', 'CONSTANTS'];
 
-  function appRun($rootScope, $state, TcAuthService, $cookies, Helpers, $log, NotificationService) {
+  function appRun($rootScope, $state, TcAuthService, $cookies, Helpers, $log, NotificationService, $window, CONSTANTS) {
     // Attaching $state to the $rootScope allows us to access the
     // current state in index.html (see div with ui-view on the index page)
     $rootScope.$state = $state;
+
+    $window.NEW_RELIC_APPLICATION_ID = CONSTANTS.NEW_RELIC_APPLICATION_ID;
 
     // check AuthNAuth on change state start
     $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
