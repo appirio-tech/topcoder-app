19d7c395258cd1bd6bb9192fb8b07f218fd81488
Nick Litwin <nlitwin@appirio.com>
2015-08-26 17:03:39 -0700

    Implement update profile image

--- app/services/image.service.js
+++ app/services/image.service.js
@@ -9,11 +9,17 @@
     var api = ApiService.restangularV3;
 
     var service = {
+      createFileRecord: createFileRecord,
+      getPresignedUrl: getPresignedUrl
     };
     return service;
 
-    function getChallenges(params) {
-      return api.all('challenges').getList(params);
+    function createFileRecord(username, body) {
+      return api.one('members', username).one('photo').customPUT(JSON.stringify(body));
+    }
+
+    function getPresignedUrl(username) {
+      return api.one('members', username).one('photoUploadUrl').post();
     }
   };
 
--- app/settings/edit-profile/edit-profile.controller.js
+++ app/settings/edit-profile/edit-profile.controller.js
@@ -3,13 +3,15 @@
 
   angular.module('tc.settings').controller('EditProfileController', EditProfileController);
 
-  EditProfileController.$inject = ['userData', 'userHandle', 'ProfileService', '$log', 'ISO3166'];
+  EditProfileController.$inject = ['userData', 'userHandle', 'ProfileService', '$log', 'ISO3166', 'ImageService'];
 
-  function EditProfileController(userData, userHandle, ProfileService, $log, ISO3166) {
+  function EditProfileController(userData, userHandle, ProfileService, $log, ISO3166, ImageService) {
     var vm = this;
-    vm.updateProfile  = updateProfile;
     vm.toggleTrack    = toggleTrack;
-    vm.updateCountry = updateCountry;
+    vm.updateCountry  = updateCountry;
+    vm.onFileChange   = onFileChange;
+    vm.updateImage    = updateImage;
+    vm.updateProfile  = updateProfile;
 
     vm.countries = ISO3166.getAllCountryObjects();
     vm.countryObj = ISO3166.getCountryObjFromAlpha3(userData.competitionCountryCode);
@@ -23,6 +25,66 @@
     function activate() {
       processData(userData);
       vm.userData = userData.plain();
+    }
+
+
+    function toggleTrack(track) {
+      vm.tracks[track] = !vm.tracks[track];
+    }
+
+    function updateCountry(angucompleteCountryObj) {
+        var countryCode = _.get(angucompleteCountryObj, 'originalObject.alpha3', undefined);
+        vm.userData.competitionCountryCode = countryCode;
+
+        var valid = _.isUndefined(countryCode) ? false : true;
+        vm.editProfile.location.$setValidity('required', valid);
+    }
+
+    function onFileChange(file) {
+      ImageService.getPresignedUrl(userHandle)
+      .then(function(res) {
+        var xhr = new XMLHttpRequest();
+        var formData = new FormData();
+        formData.append(file.name, file);
+
+        xhr.onreadystatechange = function() {
+          var status = xhr.status;
+          if ((status >= 200 && status < 300) || status === 304) {
+            ImageService.createFileRecord(userHandle, {param: {token: res.token}})
+            .then(function(res) {
+              $log.info('Successfully made file record.');
+            })
+            .catch(function(err) {
+              $log.info('Error in creating file record');
+              $log.error(err);
+            });
+          }
+        };
+
+        xhr.onerror = function(res) {
+          $log.info('Error uploading to s3');
+          $log.error(res);
+        }
+
+        xhr.open('PUT', res.preSignedURL, true);
+        xhr.setRequestHeader('Content-Type', 'multipart/form-data');
+        xhr.send(formData);
+      })
+      .catch(function(err) {
+        $log.info('Error getting presigned url');
+        $log.error(err);
+      });
+    }
+
+    function updateImage() {
+      ImageService.updateImage()
+      .then(function(res) {
+        vm.token = res.token;
+        vm.presignedUrl = res.presignedUrl;
+      })
+      .catch(function(err){
+        $log.error(err);
+      });
     }
 
     function updateProfile() {
@@ -42,19 +104,6 @@
       .catch(function(err) {
         $log.error(err);
       });
-    }
-
-    function toggleTrack(track) {
-      vm.tracks[track] = !vm.tracks[track];
-    }
-
-    function updateCountry(angucompleteCountryObj) {
-        // update country
-        var countryCode = _.get(angucompleteCountryObj, 'originalObject.alpha3', undefined);
-        vm.userData.competitionCountryCode = countryCode;
-
-        var valid = _.isUndefined(countryCode) ? false : true;
-        vm.editProfile.location.$setValidity('required', valid);
     }
 
     function processData(userInfo) {
