64bdab2d88c74071ebdc189761dad3b7ca36f207
Thabo <tha.bo.fletcher@gmail.com>
2015-09-12 23:32:19 -0700

    fix login/logout

--- app/topcoder.controller.js
+++ app/topcoder.controller.js
@@ -8,7 +8,6 @@
   function TopcoderController(NotificationService, $rootScope, CONSTANTS) {
     var vm = this;
     vm.menuVisible = false;
-    vm.loggedIn = false;
     // set some $rootScope constants here
     $rootScope.DOMAIN = CONSTANTS.domain;
 
