f91214410c8e066a6920b2681c940e42a93c5c1c
Nick Litwin <nlitwin@appirio.com>
2015-09-13 11:13:16 -0700

    Move call back to controller

--- app/settings/settings.controller.js
+++ app/settings/settings.controller.js
@@ -3,22 +3,14 @@
 
   angular.module('tc.settings').controller('SettingsController', SettingsController);
 
-  SettingsController.$inject = ['UserService'];
+  SettingsController.$inject = [];
 
-  function SettingsController(UserService) {
+  function SettingsController() {
     var vm = this;
 
     activate();
 
     function activate() {
-      UserService.getUserProfile({fields: 'credential'})
-      .then(function(res) {
-        vm.isSocialRegistrant = res.credential.hasPassword;
-      })
-      .catch(function(err) {
-        $log.error("Error fetching user profile. Redirecting to edit profile.");
-        $state.go('settings.profile');
-      });
     }
   }
 
