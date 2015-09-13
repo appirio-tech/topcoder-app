3391f98ee93cbf3441ba8a847dec6351acd6d2ad
Nick Litwin <nlitwin@appirio.com>
2015-09-13 10:43:41 -0700

    Show update password fields if user is not a social registrant

--- app/settings/settings.controller.js
+++ app/settings/settings.controller.js
@@ -3,14 +3,22 @@
 
   angular.module('tc.settings').controller('SettingsController', SettingsController);
 
-  SettingsController.$inject = [];
+  SettingsController.$inject = ['UserService'];
 
-  function SettingsController() {
+  function SettingsController(UserService) {
     var vm = this;
 
     activate();
 
     function activate() {
+      UserService.getUserProfile({fields: 'credential'})
+      .then(function(res) {
+        vm.isSocialRegistrant = res.credential.hasPassword;
+      })
+      .catch(function(err) {
+        $log.error("Error fetching user profile. Redirecting to edit profile.");
+        $state.go('settings.profile');
+      });
     }
   }
 
