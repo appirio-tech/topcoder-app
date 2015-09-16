5fcde8f60f72df348b2c4a2845329f008c9d7a33
Nick Litwin <nlitwin@appirio.com>
2015-09-16 15:39:54 -0700

    Implement basic horizontal scrolling on mobile

--- app/directives/challenge-tiles/challenge-tile.directive.js
+++ app/directives/challenge-tiles/challenge-tile.directive.js
@@ -22,8 +22,6 @@
 
         activate();
 
-        console.log('CHALLENGE: ', $scope.challenge.plain());
-
         function activate() {
           if (!$attrs.spotlight) {
             processChallenge($scope.challenge);
@@ -31,26 +29,10 @@
         }
 
         function processChallenge(challenge) {
-          $scope.subTrack = challenge.subTrack.replace('_', ' ');
-
-          var now = moment();
-          var registrationDate = moment(challenge.registrationEndDate);
-          var submissionDate = moment(challenge.submissionEndDate);
-
-          challenge.registrationClosed = now > registrationDate ? true : false;
-          challenge.submissionClosed = now > submissionDate ? true : false;
-          challenge.registrationTimeLeft = (registrationDate - now)/(24*60*60*1000);
-          challenge.submissionTimeLeft = (submissionDate - now)/(24*60*60*1000);
-          // temporary...right now many challenges have a `null` track
-          challenge.track = challenge.track ? challenge.track.trim() : 'DESIGN';
-          if (challenge.track == 'DESIGN' && challenge.userDetails.submissions && challenge.userDetails.submissions.length > 0) {
-            challenge.thumbnailId = challenge.userDetails.submissions[0].id;
-          }
-
-          // challenge.phaseMsg = preparePhaseMessage(challenge);
-
-          // TODO create msg dynamically
-          challenge.memberStatusMsg = 'You are registered!';
+          $scope.subTrack = challenge.subTrack.replace(/_/g, ' ');
+          // if (challenge.track == 'DESIGN' && challenge.userDetails.submissions && challenge.userDetails.submissions.length > 0) {
+          //   challenge.thumbnailId = challenge.userDetails.submissions[0].id;
+          // }
         }
       }]
     };
