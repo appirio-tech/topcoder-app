2e0f41fef63a24dcff398fd77c2364f167a951a3
Parth Shah <parth.shah@appirio.com>
2015-09-16 23:39:25 -0700

    handling empty state for skills & external links

--- app/profile/about/about.controller.js
+++ app/profile/about/about.controller.js
@@ -5,7 +5,7 @@
 
   ProfileAboutController.$inject = ['$log', '$scope', 'ProfileService', 'ExternalAccountService'];
 
-  function ProfileAboutController($log, $scope, ProfileService, ExternalAccountService) {
+  function ProfileAboutController($log, $scope, ProfileService, ExternalAccountService, UserService) {
     var vm = this;
     $log = $log.getInstance("ProfileAboutController");
 
@@ -13,20 +13,27 @@
     vm.categoryIndex = 0;
     vm.skillIndex = 0;
     vm.imgMap = {};
+    vm.displaySection = {};
 
     activate();
 
     function activate() {
-      ExternalAccountService.getLinkedExternalLinksData(profileVm.userHandle)
-      .then(function(data) {
-        vm.linkedExternalAccountsData = data.plain();
+
+      profileVm.externalLinksPromise.then(function() {
+        vm.linkedExternalAccountsData = profileVm.linkedExternalAccountsData;
+        // show section if user is viewing his/her own profile OR if we have data
+        vm.hasLinks = _.any(_.valuesIn(_.omit(vm.linkedExternalAccountsData, ['userId', 'updatedAt','createdAt','createdBy','updatedBy','handle'])));
+        vm.displaySection.externalLinks = profileVm.showEditProfileLink || vm.hasLinks;
       });
 
       profileVm.statsPromise.then(function() {
         vm.categories = profileVm.categories;
       });
+
       profileVm.skillsPromise.then(function() {
+        // show section if user is viewing his/her own profile OR if we have data
         vm.skills = profileVm.skills;
+        vm.displaySection.skills = profileVm.showEditProfileLink || !!vm.skills.length;
       });
     }
 
