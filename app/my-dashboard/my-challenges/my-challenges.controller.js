c564f5afcff838684e1b1d95e12510a8021b32c1
Nick Litwin <nlitwin@appirio.com>
2015-09-24 16:08:06 -0700

    In progress code while dev is down

--- app/my-dashboard/my-challenges/my-challenges.controller.js
+++ app/my-dashboard/my-challenges/my-challenges.controller.js
@@ -3,13 +3,12 @@
 
   angular.module('tc.myDashboard').controller('MyChallengesWidgetController', MyChallengesWidgetController);
 
-  MyChallengesWidgetController.$inject = ['ChallengeService', 'UserService', '$log', 'CONSTANTS', 'userIdentity'];
+  MyChallengesWidgetController.$inject = ['ChallengeService', 'UserService', '$log', 'CONSTANTS', 'userIdentity', '$q'];
 
-  function MyChallengesWidgetController(ChallengeService, UserService, $log, CONSTANTS, userIdentity) {
+  function MyChallengesWidgetController(ChallengeService, UserService, $log, CONSTANTS, userIdentity, $q) {
     var vm = this;
     vm.domain = CONSTANTS.domain;
     vm.loading = true;
-    vm.myChallenges = [];
     vm.userHasChallenges = true;
 
     var handle = userIdentity.handle;
@@ -17,10 +16,6 @@
     activate();
 
     function activate() {
-      viewActiveChallenges();
-    }
-
-    function viewActiveChallenges() {
       vm.myChallenges = [];
       getChallenges('active', 'submissionEndDate');
     }
@@ -35,17 +30,29 @@
         }
       };
 
-      ChallengeService.getUserChallenges(handle, params)
-      .then(function(challenges){
-        ChallengeService.processActiveDevDesignChallenges(challenges);
-        // console.log(challenges.plain());
+      $q.all([
+        ChallengeService.getUserMarathonMatches(handle, params),
+        ChallengeService.getUserChallenges(handle, params)
+      ]).then(function(challenges){
+        var marathonMatches = challenges[0];
+        var devDesignChallenges = challenges[1];
 
-        if (challenges.length > 0) {
-          vm.myChallenges = challenges;
-          vm.userHasChallenges = true;
-          vm.loading = false;
-        } else {
+        // console.log('MMs: ', marathonMatches.plain());
+        // console.log('dev design challenges: ', devDesignChallenges.plain());
+
+        if (!marathonMatches.length && !devDesignChallenges.length) {
           vm.userHasChallenges = false;
+          vm.loading = false;
+
+        } else {
+          ChallengeService.processActiveDevDesignChallenges(devDesignChallenges);
+          ChallengeService.processActiveMarathonMatches(marathonMatches);
+          var userChallenges = marathonMatches.concat(devDesignChallenges);
+          // sort by closest deadline .sort(function)
+          // limit to 8 .slice(0, 8);
+
+          vm.myChallenges = userChallenges;
+          vm.userHasChallenges = true;
           vm.loading = false;
         }
       })
--- app/services/challenge.service.js
+++ app/services/challenge.service.js
@@ -3,11 +3,9 @@
 
   angular.module('tc.services').factory('ChallengeService', ChallengeService);
 
-  ChallengeService.$inject = ['CONSTANTS', 'ApiService', '$q'];
+  ChallengeService.$inject = ['CONSTANTS', 'ApiService'];
 
-  function ChallengeService(CONSTANTS, ApiService, $q) {
-
-    var rApi = ApiService.restangularV3;
+  function ChallengeService(CONSTANTS, ApiService) {
     var api = ApiService.restangularV3;
 
     var service = {
@@ -16,8 +14,10 @@
       getiOSChallenges: getiOSChallenges,
       getReviewEndDate: getReviewEndDate,
       getChallengeDetails: getChallengeDetails,
-      processActiveDevDesignChallenges: processActiveDevDesignChallenges
+      processActiveDevDesignChallenges: processActiveDevDesignChallenges,
+      processActiveMarathonMatches: processActiveMarathonMatches
     };
+
     return service;
 
     function getChallenges(params) {
@@ -28,6 +28,10 @@
     function getUserChallenges(handle, params) {
       params.filter = _parseFilterParam(params);
       return api.one('members', handle).all('challenges').getList(params);
+    }
+
+    function getUserMarathonMatches(handle, params) {
+      return api.all('members', handle).all('mms').getList(params);
     }
 
     function getiOSChallenges(params) {
@@ -81,7 +85,50 @@
 
         if (challenge.userCurrentPhaseEndTime) {
           var timeAndUnit = moment(challenge.userCurrentPhaseEndTime).fromNow(true);
+          // Split into components: ['an', 'hour'] || ['2', months]
           timeAndUnit = timeAndUnit.split(' ');
+
+          if (timeAndUnit[0] === 'a' || timeAndUnit[0] === 'an') {
+            timeAndUnit[0] = '1';
+          }
+          challenge.userCurrentPhaseEndTime = timeAndUnit;
+        }
+      });
+    }
+
+    function processActiveMarathonMatches(marathonMatches) {
+      angular.forEach(marathonMatches, function(match) {
+        var rounds = match.rounds;
+        var hasCurrentRound = false;
+
+        match.userCurrentPhase = 'Stalled';
+        match.userCurrentPhaseEndTime = null;
+        match.userAction = null;
+
+        if (rounds && rounds.length) {
+          hasCurrentRound = true;
+          match.userCurrentPhase = 'Registration';
+          match.userCurrentPhaseEndTime = rounds[0].registrationEndAt;
+          match.userAction = 'Registered';
+
+          if (moment().isAfter(rounds[0].codingStartAt)) {
+            match.userCurrentPhase = 'Coding';
+            match.userCurrentPhaseEndTime = rounds[0].codingEndAt;
+            match.userAction = 'Submit';
+          }
+
+          if (moment().isAfter(rounds[0].systemTestStartAt)) {
+            match.userCurrentPhase = 'System Test Phase';
+            match.userCurrentPhaseEndTime = rounds[0].systemTestEndAt;
+            match.userAction = null;
+          }
+        }
+
+        if (challenge.userCurrentPhaseEndTime) {
+          var timeAndUnit = moment(challenge.userCurrentPhaseEndTime).fromNow(true);
+          // Split into components: ['an', 'hour'] || ['2', months]
+          timeAndUnit = timeAndUnit.split(' ');
+
           if (timeAndUnit[0] === 'a' || timeAndUnit[0] === 'an') {
             timeAndUnit[0] = '1';
           }
