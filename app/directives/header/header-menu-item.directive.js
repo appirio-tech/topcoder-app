2326790691e7f91ae000120c3eb38ecaeaca5705
Thabo <tha.bo.fletcher@gmail.com>
2015-09-25 00:26:10 -0700

    cleanup

--- app/directives/header/header-menu-item.directive.js
+++ app/directives/header/header-menu-item.directive.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
   angular.module('tcUIComponents').directive('headerMenuItem', function() {
-    
+
     return {
       restrict: 'E',
       templateUrl: 'directives/header/header-menu-item.directive.html',
@@ -10,12 +10,12 @@
       },
       controller: ['$scope', '$state', function($scope, $state) {
         var sref = $scope.item.sref;
-        var href = $scope.item.href; // = $scope.item.srefParams ? $state.href($scope.item.sref, $scope.item.srefParams) : $scope.item.href;
-        
+        var href = $scope.item.href;
+
         // I believe I have to hack this because of https://github.com/angular-ui/ui-router/issues/395, I tried ui-state
         if ($scope.item.srefParams)
           $scope.wtfhref = $state.href($scope.item.sref, $scope.item.srefParams);
-   
+
         $scope.isActive = function() {
           if (window.location.pathname == href || $state.is(sref))
             return true;
