22ec716adb03e6cb727b163c8bf4ec2cc506eccf
Tom Ladendorf <tom@tomladendorf.com>
2015-10-06 11:25:13 -0500

    Added def val to empty filter

--- app/filters/empty.filter.js
+++ app/filters/empty.filter.js
@@ -4,11 +4,11 @@
   angular.module('topcoder').filter('empty', empty);
 
   function empty() {
-    return function(str) {
+    return function(str, defaultVal) {
       if (str === 0) {
         return str;
       } else if (!str || (typeof str === 'number' && isNaN(str)) || (typeof str === 'string' && (str.length === 0 || str.indexOf('NaN') >= 0 || str === '%'))) {
-        return '-';
+        return defaultVal || '-';
       } else {
         return str;
       }
