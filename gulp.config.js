4ce22afc2b69853e3b2ae61f62ec75ae88fef16b
Nick Litwin <nlitwin@appirio.com>
2015-10-06 17:30:30 -0700

    Fix environment setting for local

--- gulp.config.js
+++ gulp.config.js
@@ -86,8 +86,8 @@ module.exports = function() {
     },
 
     // Process.env variables
-    production: process.env.ENVIRONMENT.indexOf('production') > -1,
-    qa: process.env.ENVIRONMENT.indexOf('qa') > -1
+    production: isEnvironment('production'),
+    qa: isEnvironment('qa')
   };
 
   config.getWiredepDefaultOptions = function () {
@@ -132,5 +132,13 @@ module.exports = function() {
     };
     options.preprocessors[app + '**/!(*.spec)+(.js)'] = ['coverage'];
     return options;
+  }
+
+  function isEnvironment(env) {
+    if (process.env && process.env.ENVIRONMENT) {
+      return process.env.ENVIRONMENT.indexOf(env) > -1;
+    }
+
+    return false;
   }
 };
