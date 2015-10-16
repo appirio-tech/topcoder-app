c7a0c1c53846e348cdda047ad00590da4f24fae6
vikasrohit <vikas.agarwal@appirio.com>
2015-10-16 16:04:45 -0700

    Revert "SUP-2086, 3rd Auto help is being displayed wrong."
    This reverts commit 11b5b8ee1a3b1abe01f027dfbe5710f125b842af.

---
+++ app/my-dashboard/my-dashboard.controller.js
@@ -0,0 +1,37 @@
+(function () {
+  'use strict';
+
+  angular.module('tc.myDashboard').controller('MyDashboardController', MyDashboardController);
+
+  MyDashboardController.$inject = ['userIdentity', 'ProfileService', '$log'];
+
+  function MyDashboardController(userIdentity, ProfileService, $log) {
+    var vm = this;
+
+    activate();
+
+    function activate() {
+      vm.showSRMs = false;
+      vm.isCopilot = _.includes(userIdentity.roles, 'copilot');
+
+      displaySRMsBasedOnTrack();
+    }
+
+    function displaySRMsBasedOnTrack() {
+      ProfileService.getUserProfile(userIdentity.handle)
+      .then(function(userProfile) {
+        var isDesigner = _.includes(userProfile.tracks, 'DESIGN');
+
+        if (isDesigner && userProfile.tracks.length === 1) {
+          vm.showSRMs = false;
+        } else {
+          vm.showSRMs = true;
+        }
+      })
+      .catch(function(err) {
+        vm.showSRMs = false;
+        $log.error(err);
+      })
+    }
+  }
+})();
