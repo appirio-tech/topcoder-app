16b9202ae93fda2f7b8b8759266f1117d0dae451
Parth Shah <parth.shah@appirio.com>
2015-10-15 18:40:03 -0700

    colors for handles

--- app/directives/profile-widget/profile-widget.directive.js
+++ app/directives/profile-widget/profile-widget.directive.js
@@ -1,9 +1,9 @@
 (function() {
   'use strict';
 
-  angular.module('tcUIComponents').directive('profileWidget', ['CONSTANTS', profileWidget]);
+  angular.module('tcUIComponents').directive('profileWidget', ['CONSTANTS', '$filter', profileWidget]);
 
-  function profileWidget(CONSTANTS) {
+  function profileWidget(CONSTANTS, $filter) {
     return {
       restrict: 'E',
       templateUrl: 'directives/profile-widget/profile-widget.html',
@@ -16,7 +16,9 @@
       link: function(scope, elem, attrs) {
         scope.DOMAIN = CONSTANTS.domain;
         scope.ASSET_PREFIX = CONSTANTS.ASSET_PREFIX;
-
+        // get max rating or default to 0
+        var rating = _.get(scope.profile, 'maxRating.rating', 0);
+        scope.handleColor = $filter('ratingColor')(rating);
         scope.$watch('editProfileLink', function(newValue, oldValue, scope) {
           if (newValue) {
             scope.editProfileLink = newValue;
