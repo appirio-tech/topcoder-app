a3a25b091dc4ba1514cb9a2c7c786d8a03943636
vikasrohit <vikas.agarwal@appirio.com>
2015-10-20 20:11:41 -0700

    SUP-1889, Dashboard UI Polish
    -- Fixed unit tests

--- app/my-dashboard/header-dashboard/header-dashboard.spec.js
+++ app/my-dashboard/header-dashboard/header-dashboard.spec.js
@@ -116,7 +116,8 @@ describe('Header Dashboard Controller', function() {
         NotificationService : notificationService,
         UserService : userService,
         ProfileService: profileService,
-        userIdentity: identity
+        userIdentity: identity,
+        profile: profile
       });
       $rootScope.$apply();
     });
@@ -146,7 +147,8 @@ describe('Header Dashboard Controller', function() {
         NotificationService : notificationService,
         UserService : userService,
         ProfileService: profileService,
-        userIdentity: identity
+        userIdentity: identity,
+        profile: profile
       });
       $rootScope.$apply();
     });
