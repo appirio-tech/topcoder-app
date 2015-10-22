69263ae3036bfe730bd93f48e1ea499923ee29ba
Tom Ladendorf <tom@tomladendorf.com>
2015-10-22 18:05:58 -0500

    fixed mistake

--- app/services/nav.service.js
+++ app/services/nav.service.js
@@ -67,8 +67,11 @@
     });
 
     function getParent(ref) {
-      if (ref.indexOf(-1) >= 0)
+      if (ref.indexOf('.') >= 0)
         ref = ref.slice(0, ref.indexOf('.'));
+
+      console.log('REF')
+      console.log(ref);
       if (ref.match(/profile/)) {
         if (TcAuthService.isAuthenticated() && $state.params && $state.params.userHandle == UserService.getUserIdentity().handle) {
           return 'user';
