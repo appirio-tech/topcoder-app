5e7f66ec2ab85acb0cea33855d492b7f3805dea0
vikasrohit <vikas.agarwal@appirio.com>
2015-10-26 15:42:51 -0700

    SUP-2231, New Empty States for Dashboard and Profile
    -- Refactored the empty state directive to accommodate dynamic content html between title and description
    -- Created service for empty state data
    -- Added support for themes in empty state directive to avoid any css set in parent page's css
    -- Added empty states for profile page (skills, activity, external links)

--- app/directives/empty-state-placeholder/empty-state-placeholder.directive.js
+++ app/directives/empty-state-placeholder/empty-state-placeholder.directive.js
@@ -4,30 +4,47 @@
   angular.module('tcUIComponents')
     .directive('emptyStatePlaceholder', emptyStatePlaceholder);
 
-  function emptyStatePlaceholder() {
+  emptyStatePlaceholder.$inject = ['CONSTANTS', 'EmptyStateService'];
+
+  function emptyStatePlaceholder(CONSTANTS, EmptyStateService) {
     return {
       restrict: 'E',
       transclude: true,
       templateUrl: 'directives/empty-state-placeholder/empty-state-placeholder.directive.html',
       scope: {
-        title: '@',
-        description: '@',
-        show: '='
+        show: '=',
+        stateName: '@',
+        theme: '@'
       },
-      controller: ['$scope', 'CONSTANTS', '$attrs',
-        function($scope, CONSTANTS, $attrs) {
+      link : function(scope, element, attrs) {
+        var rootDiv = angular.element(element.children()[0]);
+        var contentDiv = _.find(rootDiv.children(), function(el) {
+          return angular.element(el).hasClass("content");
+        });
+        scope.transcluded = angular.element(contentDiv)[0].children.length > 0;
+      },
+      controller: ['$scope', 'CONSTANTS', '$attrs', '$element',
+        function($scope, CONSTANTS, $attrs, $element) {
           $scope.DOMAIN = CONSTANTS.domain;
           var vm = this;
-          vm.title = $scope.title;
-          vm.description = $scope.description;
+          vm.title = null;
+          vm.description = null;
+          vm.theme = _.get($scope, 'theme', null);
+          vm.helpLinks = null;
           vm.show = _.get($scope, 'show', false);
 
           activate();
 
           function activate() {
-            $scope.$watch('show', function (newValue, oldValue) {
-              vm.show = newValue;
-            })
+            var state = EmptyStateService.getState($scope.stateName);
+            if (state) {
+              vm.title = state.title;
+              vm.description = state.description;
+              vm.helpLinks = state.helpLinks;
+              $scope.$watch('show', function (newValue, oldValue) {
+                vm.show = newValue;
+              });
+            }
           }
       }],
       controllerAs: "vm"
---
+++ app/services/emptyState.service.js
@@ -0,0 +1,140 @@
+(function() {
+  'use strict';
+
+  angular.module('tc.services').factory('EmptyStateService', EmptyStateService);
+
+  EmptyStateService.$inject = ['CONSTANTS'];
+
+  function EmptyStateService(CONSTANTS) {
+    var states = null;
+    var domain = CONSTANTS.domain;
+    _init();
+
+    var service = {
+      getState: getState
+    };
+
+    return service;
+
+    function getState(stateName) {
+      if (states[stateName]) {
+        return states[stateName];
+      }
+      return null;
+    }
+
+    function _getUrl (path) {
+      return "https://www." + domain + path;
+    }
+
+    function _getIOSUrl(path) {
+      return "https://ios." + domain + path;
+    }
+
+    function _init() {
+      states = {
+        "dashboard-challenges": {
+          title: "My Challenges",
+          description: "You don't have any active challenges. Ready to find your next challenge?",
+          helpLinks: [{
+            title: "Explore Open Challenges",
+            state: "my-challenges({status: 'completed'})",
+            cssClass: "find-challenges tc-btn tc-btn-s"
+          }]
+        },
+        "dashboard-ios-community": {
+          title: "iOS COMMUNITY",
+          description: "Earn iOS topcoder badges and exclusive access to iOS challenges, prizes and special community-related events.",
+          helpLinks: [{
+            title: "Participate",
+            onClick: "vm.registerUser()",
+            cssClass: "participate tc-btn tc-btn-s tc-btn-ghost"
+          }, {
+            title: "Learn More",
+            url: _getIOSUrl("/"),
+            cssClass: "learn-more"
+          }]
+        },
+        "dashboard-challenges-never-participated": {
+          title: "My Challenges",
+          description: "Compete in challenges to win money, test yourself against the world's best, and learn new skills",
+          helpLinks: [{
+            title: "Find Challenges",
+            url: _getUrl("/challenges/?pageIndex=1"),
+            cssClass: "find-challenges tc-btn tc-btn-s"
+          }, {
+            title: "Learn More",
+            url: _getUrl("/member-onboarding"),
+            cssClass: "learn-more"
+          }]
+        },
+        "my-challenges-active": {
+          title: "Active Challenges",
+          description: "You don't have any active challenges. Ready to find your next challenge?",
+          helpLinks: [{
+            title: "Explore Open Challenges",
+            state: "my-challenges({status: 'completed'})",
+            cssClass: "find-challenges tc-btn tc-btn-s tc-btn-ghost"
+          }]
+        },
+        "my-challenges-past": {
+          title: "Past Challenges",
+          description: "You have not participated in any challenges yet.",
+          helpLinks: [{
+            title: "Find Challenges",
+            url: _getUrl("/challenges/?pageIndex=1"),
+            cssClass: "find-challenges tc-btn tc-btn-s tc-btn-ghost"
+          }, {
+            title: "Learn More",
+            url: _getUrl("/member-onboarding"),
+            cssClass: "learn-more"
+          }]
+        },
+        "my-challenges-never-participated": {
+          title: "My Challenges",
+          description: "Compete in challenges to win money, test yourself against the world's best, and learn new skills",
+          helpLinks: [{
+            title: "Find Challenges",
+            url: _getUrl("/challenges/?pageIndex=1"),
+            cssClass: "find-challenges tc-btn tc-btn-s tc-btn-ghost"
+          }, {
+            title: "Learn More",
+            url: _getUrl("/member-onboarding"),
+            cssClass: "learn-more"
+          }]
+        },
+        "profile-topcoder-activity": {
+          title: "My Challenges",
+          description: "You have not participated in any challenges yet.",
+          helpLinks: [{
+            title: "Explore Challenges",
+            url: _getUrl("/challenges/?pageIndex=1"),
+            cssClass: "find-challenges tc-btn tc-btn-s tc-btn-ghost"
+          }, {
+            title: "Learn More",
+            url: _getUrl("/member-onboarding"),
+            cssClass: "learn-more"
+          }]
+        },
+        "profile-skills": {
+          title: "Skills",
+          description: "You can add languages, environments, frameworks, libraries, platforms, tools, and any other technologies that you know well.",
+          helpLinks: [{
+            title: "Add Your Skills",
+            state: "settings.profile",
+            cssClass: "add-skills tc-btn tc-btn-s tc-btn-ghost"
+          }]
+        },
+        "profile-external-links": {
+          title: "External Links",
+          description: "You can add languages, environments, frameworks, libraries, platforms, tools, and any other technologies that you know well.",
+          helpLinks: [{
+            title: "Connect Your Accounts",
+            state: "settings.profile",
+            cssClass: "connect-external-accounts tc-btn tc-btn-s tc-btn-ghost"
+          }]
+        }
+      }
+    }
+  };
+})();
