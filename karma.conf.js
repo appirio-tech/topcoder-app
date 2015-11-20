b29753b3e22aa92876c7e93763602cfcece634f3
vikasrohit <vikas.agarwal@appirio.com>
2015-11-20 15:06:27 +0530

    SUP-2729, Publish unit test results with build
    -- Published unit test results in JUnit format.

--- karma.conf.js
+++ karma.conf.js
@@ -26,7 +26,12 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress', 'coverage'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
+    reporters: ['junit', 'progress', 'coverage'],
+
+    junitReporter: {
+      outputDir: 'report/junit',
+      outputFile: 'test-results.xml',
+    },
 
     coverageReporter: {
       dir: gulpConfig.karma.coverage.dir,
