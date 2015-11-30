c8c65baa79f36a9635944f15a1e2da78e5236235
Nick Litwin <nlitwin@appirio.com>
2015-11-30 12:00:33 -0800

    Simplify to one clean

--- gulp-tasks/clean.js
+++ gulp-tasks/clean.js
@@ -8,13 +8,4 @@ module.exports = function(gulp, $, config, utilities) {
     utilities.log('Cleaning: ' + $.util.colors.blue(delconfig));
     del(delconfig, done);
   });
-  gulp.task('clean-fonts', function(done) {
-    utilities.clean(config.build + 'fonts/**/*.*', done);
-  });
-  gulp.task('clean-images', function(done) {
-    utilities.clean(config.build + 'images/**/*.*', done);
-  });
-  gulp.task('clean-styles', function(done) {
-    utilities.clean(config.temp + '**/*.css', done);
-  });
 };
