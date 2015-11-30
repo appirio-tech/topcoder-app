abf7bf450e6ce8da806761c2ce4306734817a237
Nick Litwin <nlitwin@appirio.com>
2015-11-30 12:00:51 -0800

    Move images out to own file

--- gulp-tasks/images.js
+++ gulp-tasks/images.js
@@ -1,5 +1,28 @@
 module.exports = function(gulp, $, config, utilities) {
   'use strict';
 
+  gulp.task('images', ['clean'], function() {
+    utilities.log('Copying and compressing the images');
 
+    return gulp
+      .src(config.images)
+      .pipe($.imagemin({optimizationLevel: 4}))
+      .pipe(gulp.dest(config.temp + 'images'));
+  });
+
+  gulp.task('images-orig-nav', ['build1'], function() {
+    utilities.log('Copying original images');
+
+    return gulp
+       .src(config.assets + 'images/nav/**.*')
+      .pipe(gulp.dest(config.build + 'images/nav'));
+  });
+
+  gulp.task('images-orig', ['images-orig-nav'], function() {
+    utilities.log('Copying original images');
+
+    return gulp
+       .src(config.assets + 'images/skills/**.*')
+      .pipe(gulp.dest(config.build + 'images/skills'));
+  });
 };
