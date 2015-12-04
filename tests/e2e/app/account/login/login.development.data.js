69ca757e2493f15f707eac658b0f9d1530f2086a
abdul-qadir <abdul.mansoori@appirio.com>
2015-12-04 13:01:42 +0530

    dev-data file

--- tests/e2e/app/account/login/login.development.data.js
+++ tests/e2e/app/account/login/login.development.data.js
@@ -11,10 +11,10 @@ var LoginData = function() {
                            ],
                            
     this.wrongPasswordCredentials= [
-                                    {
+                                    /*{
                                     	'username' : 'aqmansuri',
                                     	'password' : 'appirio123$'
-                                    },
+                                    },*/
                                     {
                                     	'username' : 'DhananjayKumar1',
                                     	'password' : 'wrongpassword'
@@ -45,14 +45,14 @@ var LoginData = function() {
     					 ],
   						  	
   	this.googleCredentials = [ {
-  								'username' : 'topcoderpuneet4@gmail.com',
-								'password' : 'LavaIris123'
+  								'username' : 'ted@cloudworks-qa.com',
+								'password' : 'Appirio#123'
 							   }
   						 ],
   						 
   	this.gitCredentials = [ {
-  								'username' : 'auto-user9',
-  								'password' : 'appirio123$'
+  								'username' : 'abdul-auto',
+  								'password' : 'appirio123'
   								}
   							],	
   							
@@ -69,11 +69,11 @@ var LoginData = function() {
   						  	
     						  
     						  
-    this.baseUrl = 'http://www.topcoder-dev.com/login',
-    this.dashBoardUrl = 'https://www.topcoder-dev.com/my-dashboard/',
-    this.invalidPasswordMessage = 'That password doesn\'t match the one we have on file. Please try again.',
+    this.baseUrl = 'http://www.topcoder-qa.com/login',
+    this.dashBoardUrl = 'https://www.topcoder-qa.com/my-dashboard/',
+    this.invalidPasswordMessage = 'That password is incorrect. Please check that you entered the right one.',
     this.invalidHandle = 'We couldn\'t find a member with that username. Please check that you entered it correctly.',
-    this.invalidSpaceHandle = 'That password doesn\'t match the one we have on file. Please try again.',
+    this.invalidSpaceHandle = 'That password doesn\'t match the one we have on file. Please try again.'	
     
 };
 module.exports = new LoginData;
--- tests/e2e/app/account/register/register.development.data.js
+++ tests/e2e/app/account/register/register.development.data.js
@@ -9,6 +9,14 @@ var RegistrationData = function() {
                             'username' : 'user_name_'+rand,
                             'email' : 'abdul.mansoori+'+rand+'@appirio.com',
                             'password' : 'appirio123'
+                        },
+                        {	
+                            'firstname' : 'user_firstname_1'+rand,
+                            'lastname' : 'user_lastname_1'+rand,
+                            'country'  : 'India',
+                            'username' : 'user_name1'+rand,
+                            'email' : 'abdul.mansoori+'+rand+'1@appirio.com',
+                            'password' : 'appirio123'
                         }
                     ],
     this.baseUrl = 'http://app.topcoder-qa.com/register/',
@@ -17,7 +25,7 @@ var RegistrationData = function() {
     this.alreadyTakenHandleMessage = 'That username is already taken.',
     this.invalidEmailMessage = 'Please enter a valid email address.',
     this.alreadyTakenEmailMessage = 'That email address is already taken.',
-    this.regSuccess = 'Thanks for joining Topcoder.We\'ve sent you a confirmation link. Please check your email and click the link to activate your account. If you can\'t find the message, please email support@topcoder.com.',
+    this.regSuccess = 'Thanks for joining Topcoder.\nWe\'ve sent you a confirmation link. Please check your email and click the link to activate your account. If you can\'t find the message, please email support@topcoder.com.',
     
     this.invalidLengthUserNameList = [{
     							'firstname' : 'user_firstname_'+rand,
@@ -42,16 +50,33 @@ var RegistrationData = function() {
                                 'username' : 'ass',
                                 'email' : 'abdul'+rand+'@appirio.com',
                                 'password' : 'user_password_$$'+rand
-  								}
+  								},
+  								{
+  		  							'firstname' : 'user_firstname'+rand,
+  	                                'lastname' : 'user_lastname'+rand,
+  	                                'country'  : 'India',
+  	                                'username' : 'ass',
+  	                                'email' : 'abdul'+rand+'1@appirio.com',
+  	                                'password' : 'user_password_$$'+rand
+  	  								}
   							   ],
   this.alreadyTakenUserNameList = [{
 							  'firstname' : 'user_firstname_'+rand,
 						      'lastname' : 'user_lastname_'+rand,
 						      'country'  : 'India',
-						      'username' : 'abdul'+rand,
-						      'email' : 'abdul.mansoori@appirio.com',
+						      'username' : 'aqmansuri',
+						      'email' : 'abdul'+rand+'@appirio.com',
 						      'password' : 'user_password_$$'+rand
-  							}],
+  								},
+  								{
+  								  'firstname' : 'user_firstname'+rand,
+  							      'lastname' : 'user_lastname'+rand,
+  							      'country'  : 'India',
+  							      'username' : 'Iamtong',
+  							      'email' : 'abdul'+rand+'@appirio.com',
+  							      'password' : 'user_password_$'+rand
+  	  							}
+  							],
  this.invalidEmailUserNameList = [{
 	 						'firstname' : 'user_firstname_'+rand,
 						      'lastname' : 'user_lastname_'+rand,
@@ -59,13 +84,28 @@ var RegistrationData = function() {
 						      'username' : 'aq'+rand,
 						      'email' : 'abdul.mansoori',
 						      'password' : 'user_password_$$'+rand
-  							}],
+  							},{
+  		 						'firstname' : 'user_firstname_'+rand,
+  						      'lastname' : 'user_lastname_'+rand,
+  						      'country'  : 'India',
+  						      'username' : 'aq'+rand,
+  						      'email' : 'aqmansuri',
+  						      'password' : 'user_password_$$'+rand
+    							}
+  							],
  this.alreadyTakenEmailUserNameList = [{
 							 'firstname' : 'user_firstname_'+rand,
 							 'lastname' : 'user_lastname_'+rand,
-							 'country'  : '',
+							 'country'  : 'India',
 							 'username' : 'aq'+rand,
 							 'email' : 'abdul.mansoori@appirio.com',
+							 'password' : 'user_password_$$'+rand
+						 },{
+							 'firstname' : 'user_firstname_'+rand,
+							 'lastname' : 'user_lastname_'+rand,
+							 'country'  : 'India',
+							 'username' : 'aq'+rand,
+							 'email' : 'ted@cloudworks-qa.com',
 							 'password' : 'user_password_$$'+rand
 						 }];	
  this.githubUserList = [{
@@ -75,8 +115,8 @@ var RegistrationData = function() {
 		 'username' : 'aq'+rand,
 		 'email' : 'abdul.mansoori@appirio.com',
 		 'password' : 'user_password_$$'+rand,
-		 'githubUsername' : 'abdul-auto',
-		 'githubPassword' : 'appirio123$'
+		 'githubUsername' : 'abdul-auto1',
+		 'githubPassword' : 'appirio123'
 	 }];	    
     
     
