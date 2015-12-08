79e6c95a11974deb51850d743d48e3d6d3be86c3
Nick Litwin <nlitwin@appirio.com>
2015-12-08 13:48:52 -0800

    Add submissions routes

--- app/submissions/submissions.routes.js
+++ app/submissions/submissions.routes.js
@@ -1,1 +1,54 @@
-submissions.routes.js
+(function() {
+  'use strict';
+
+  angular.module('tc.submissions').config([
+    '$stateProvider',
+    '$locationProvider',
+    routes
+  ]);
+
+  function routes($stateProvider, $locationProvider) {
+
+    // Commenting this out to see if it is necessary
+    // $locationProvider.html5Mode(true);
+
+    var states = {
+      submissions: {
+        parent: 'root',
+        abstract: false,
+        url: '/submissions/',
+        templateUrl: 'submissions/submissions.html',
+        controller: 'SubmissionsController',
+        controllerAs: 'submissions',
+        data: {
+
+          // Is Auth required?
+          authRequired: true
+        },
+        resolve: {
+
+          // userHandle: ['UserService', function(UserService) {
+          //   return UserService.getUserIdentity().handle;
+          // }],
+          // userData: ['userHandle', 'ProfileService', function(userHandle, ProfileService) {
+          //   return ProfileService.getUserProfile(userHandle);
+          // }]
+        },
+      },
+      'submissions.design': {
+        url: 'design/',
+        templateUrl: 'submissions/design/design.html',
+        controller: 'DesignSubmissionsController',
+        controllerAs: 'vm',
+        data: {
+          title: 'Design Submissions'
+        }
+      }
+    };
+
+    for (var name in states) {
+      var state = states[name];
+      $stateProvider.state(name, state);
+    }
+  }
+})();
