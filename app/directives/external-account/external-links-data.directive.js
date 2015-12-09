c1143617a6ca09d7d6d08fa4d20c15848808e32b
vikasrohit <vikas.agarwal@appirio.com>
2015-12-09 14:35:36 +0530

    SUP-2754, [Edit Profile] Allow user to hide external links
    -- Implemented code review suggestion for having separate controller for account deletion logic
    -- Added link title to the confirmation message with italic(to differentiate it from other text) font style
    -- Updated unit tests after refactoring

--- app/directives/external-account/external-links-data.directive.js
+++ app/directives/external-account/external-links-data.directive.js
@@ -22,58 +22,28 @@
         function($log, $scope, ExternalWebLinksService, toaster, ngDialog) {
 
           $log = $log.getInstance("ExternalLinksDataCtrl");
-          $scope.toDelete = null;
           $scope.deletionDialog = null;
 
           $scope.confirmDeletion = function(account) {
-            $scope.toDelete = account;
             $scope.deletionDialog = ngDialog.open({
               className: 'ngdialog-theme-default tc-dialog',
               template: 'directives/external-account/external-link-deletion-confirm.html',
-              scope: $scope
+              controller: 'ExternalLinkDeletionController',
+              controllerAs: 'vm',
+              resolve: {
+                userHandle: function() {
+                  return $scope.userHandle;
+                },
+                account: function() {
+                  return account;
+                },
+                linkedAccountsData: function() {
+                  return $scope.linkedAccountsData;
+                }
+              }
             }).closePromise.then(function (data) {
               $log.debug('Closing deletion confirmation dialog.');
-              $scope.toDelete = null;
             });
-          }
-
-          $scope.deleteAccount = function() {
-            $log.debug('Deleting Account...');
-            var account = $scope.toDelete;
-            if (account && account.deletingAccount) {
-              $log.debug('Another deletion is already in progress.');
-              return;
-            }
-            if (account && account.provider === 'weblink') {
-              account.deletingAccount = true;
-              $log.debug('Deleting weblink...');
-              return ExternalWebLinksService.removeLink($scope.userHandle, account.key).then(function(data) {
-                $scope.deletingAccount = false;
-                $log.debug("Web link removed: " + JSON.stringify(data));
-                var toRemove = _.findIndex($scope.linkedAccountsData, function(la) {
-                  return la.provider === 'weblink' && la.key === account.key;
-                });
-                if (toRemove > -1) {
-                  // remove from the linkedAccountsData array
-                  $scope.linkedAccountsData.splice(toRemove, 1);
-                }
-                account.deletingAccount = false;
-                toaster.pop('success', "Success", "Your link has been removed.");
-              })
-              .catch(function(resp) {
-                var msg = resp.msg;
-                if (resp.status === 'WEBLINK_NOT_EXIST') {
-                  $log.info("Weblink does not exist");
-                  msg = "Weblink is not linked to your account. If you think this is an error please contact <a href=\"mailTo:support@topcoder.com\">support@topcoder.com</a>.";
-                } else {
-                  $log.error("Fatal error: _unlink: " + msg);
-                  msg = "Sorry! We are unable to remove your weblink. If problem persists, please contact <a href=\"mailTo:support@topcoder.com\">support@topcoder.com</a>";
-                }
-
-                account.deletingAccount = false;
-                toaster.pop('error', "Whoops!", msg);
-              });
-            }
           }
         }
       ]
--- app/directives/external-account/external-links-data.directive.spec.js
+++ app/directives/external-account/external-links-data.directive.spec.js
@@ -1,8 +1,8 @@
 /* jshint -W117, -W030 */
-xdescribe('External Links Data Directive', function() {
+describe('External Links Data Directive', function() {
   var scope;
   var element;
-  var toasterSvc, extLinkSvc, ngDialogSvc;
+  var ngDialogSvc;
   var mockLinkedAccounts = [
     {
       provider: 'github',
@@ -65,36 +65,8 @@ xdescribe('External Links Data Directive', function() {
 
   beforeEach(function() {
     bard.appModule('topcoder');
-    bard.inject(this, '$compile', '$rootScope', 'toaster', 'ExternalWebLinksService', '$q', 'ngDialog');
+    bard.inject(this, '$compile', '$rootScope', '$q', 'ngDialog');
     scope = $rootScope.$new();
-
-    extLinkSvc = ExternalWebLinksService;
-
-    sinon.stub(extLinkSvc, 'removeLink', function(handle, key) {
-      var $deferred = $q.defer();
-      if (key === 'throwNotExistsError') {
-        $deferred.reject({
-          status: 'WEBLINK_NOT_EXIST',
-          msg: 'profile not exists'
-        });
-      } else if(key === 'throwFatalError') {
-        $deferred.reject({
-          status: 'FATAL_ERROR',
-          msg: 'fatal error'
-        });
-      } else {
-        $deferred.resolve({
-          status: 'SUCCESS'
-        });
-      }
-      return $deferred.promise;
-    });
-
-    toasterSvc = toaster;
-    bard.mockService(toaster, {
-      pop: $q.when(true),
-      default: $q.when(true)
-    });
 
     ngDialogSvc = ngDialog;
     sinon.stub(ngDialog, 'open', function() {
@@ -131,92 +103,16 @@ xdescribe('External Links Data Directive', function() {
       expect(element.isolateScope().linkedAccountsData).to.have.length(8);
     });
 
-    it('should remove weblink ', function() {
-      element.isolateScope().toDelete = {key: 'somekey', provider: 'weblink'};
-      element.isolateScope().deleteAccount();
-      scope.$digest();
-      expect(toasterSvc.pop).to.have.been.calledWith('success').calledOnce;
-      expect(element.isolateScope().linkedAccountsData).to.have.length(7);
-    });
-
-    it('should show success if controller doesn\'t have weblink but API returns success ', function() {
-      element.isolateScope().toDelete = {key: 'somekey1', provider: 'weblink'};
-      element.isolateScope().deleteAccount();
-      scope.$digest();
-      expect(toasterSvc.pop).to.have.been.calledWith('success').calledOnce;
-      expect(element.isolateScope().linkedAccountsData).to.have.length(8);
-    });
-
-    it('should NOT remove weblink with fatal error ', function() {
-      element.isolateScope().toDelete = {key: 'throwFatalError', provider: 'weblink'};
-      element.isolateScope().deleteAccount();
-      scope.$digest();
-      expect(toasterSvc.pop).to.have.been.calledWith('error', "Whoops!", sinon.match('Sorry!')).calledOnce;
-      expect(element.isolateScope().linkedAccountsData).to.have.length(8);
-    });
-
-    it('should NOT remove weblink with already removed weblink ', function() {
-      element.isolateScope().toDelete = {key: 'throwNotExistsError', provider: 'weblink'};
-      element.isolateScope().deleteAccount();
-      scope.$digest();
-      expect(toasterSvc.pop).to.have.been.calledWith('error', "Whoops!", sinon.match('not linked')).calledOnce;
-      expect(element.isolateScope().linkedAccountsData).to.have.length(8);
-    });
-
-    it('should not do any thing when already a deletion is in progress ', function() {
-      element.isolateScope().toDelete = {key: 'somekey', provider: 'weblink', deletingAccount: true};
-      element.isolateScope().deleteAccount();
-      scope.$digest();
-      expect(extLinkSvc.removeLink).not.to.be.called;
-      expect(toasterSvc.pop).not.to.be.called;
-      expect(element.isolateScope().linkedAccountsData).to.have.length(8);
-    });
-
-    it('should not do any thing for non weblink provider ', function() {
-      element.isolateScope().toDelete = {key: 'somekey', provider: 'stackoverflow'};
-      element.isolateScope().deleteAccount();
-      scope.$digest();
-      expect(extLinkSvc.removeLink).not.to.be.called;
-      expect(toasterSvc.pop).not.to.be.called;
-      expect(element.isolateScope().linkedAccountsData).to.have.length(8);
-    });
-
     it('should mark the account for deletion and open the popup ', function() {
       var account = {key: 'somekey', provider: 'stackoverflow'};
       element.isolateScope().confirmDeletion(account);
       scope.$digest();
-      // just to cross check that it does not call removal service directly
-      expect(extLinkSvc.removeLink).not.to.be.called;
       // should open the popup
       expect(ngDialogSvc.open).to.be.called;
       // should not remove anythign from the array of accounts/links
       expect(element.isolateScope().linkedAccountsData).to.have.length(8);
-      // $scope.toDelete should point to account passed to the confirmDeletion method
-      expect(element.isolateScope().toDelete).to.exist;
-      expect(element.isolateScope().toDelete.key).to.exist.to.equal(account.key);
-      expect(element.isolateScope().toDelete.provider).to.exist.to.equal(account.provider);
-      ngDialogSvc.close();
-    });
-
-    it('should call closePromise after popup closing ', function() {
-      var account = {key: 'somekey', provider: 'stackoverflow'};
-      element.isolateScope().confirmDeletion(account);
-      scope.$digest();
-      // should open the popup
-      expect(ngDialogSvc.open).to.be.called;
-      // should not remove anythign from the array of accounts/links
-      expect(element.isolateScope().linkedAccountsData).to.have.length(8);
-      // $scope.toDelete should point to account passed to the confirmDeletion method
-      expect(element.isolateScope().toDelete).to.exist;
-      expect(element.isolateScope().toDelete.key).to.exist.to.equal(account.key);
-      expect(element.isolateScope().toDelete.provider).to.exist.to.equal(account.provider);
-      // adds resolve listener to the closePromise of ngDialog to verify that closePromise 
-      // has been called after closing the ngDialog
-      ngDialogSvc.deferredClose.promise.then(function() {
-        // should reset scope.toDelete to null
-        expect(element.isolateScope().toDelete).to.null;
-      });
-      // close dialog and resolve closePromise
+      // $scope.deletionDialog should exist
+      expect(element.isolateScope().deletionDialog).to.exist;
       ngDialogSvc.close();
     });
 
