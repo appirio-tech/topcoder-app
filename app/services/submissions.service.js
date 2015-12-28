17a6e2842f5e3af1938029a89371e74b721d7022
Nick Litwin <nlitwin@appirio.com>
2015-12-28 15:24:54 -0800

    Call s3 upload from presigned url success callback

--- app/services/submissions.service.js
+++ app/services/submissions.service.js
@@ -17,41 +17,38 @@
 
     return service;
 
-    function getPresignedURL(body) {
-      // var deferred = $q.defer();
-      console.log('body: ', body)
-      // return api.all('submissions').post('photoUploadUrl', {contentType: file.type})
+    function getPresignedURL(body, files) {
+      console.log('body to get presigned url: ', body)
 
       return api.all('submissions').customPOST(JSON.stringify({param: body}))
       .then(function(response) {
-        console.log('POST Response: ', response);
-        // deferred.resolve({
-        //   preSignedURL: response.preSignedURL,
-        //   token: response.token,
-        //   file: file,
-        //   userHandle: userHandle
-        // });
+        console.log('POST Response: ', response.plain());
+
+        uploadSubmissionFileToS3(response.data.files, files);
       })
       .catch(function(err) {
         console.log(err);
         $log.info('Error getting presigned url');
-        toaster.pop('error', 'Whoops!', 'There was an error uploading your profile image. Please try again later.');
-        // deferred.reject(err);
+        toaster.pop('error', 'Whoops!', 'There was an error uploading your submissions. Please try again later.');
       });
-
-      // return deferred.promise;
     }
 
-    function uploadSubmissionFileToS3(/* something that has file*/) {
-      var file;
-      var S3RequestOptions = {
-        url: 'preSignedURL',
-        method: 'PUT',
-        headers: {},
-        data: 'File Object'
-      };
+    function uploadSubmissionFileToS3(filesWithPresignedURL, files) {
+      var promises = filesWithPresignedURL.map(function(fileWithPresignedURL) {
+        var S3RequestOptions = {
+          url: fileWithPresignedURL.preSignedUploadUrl,
+          method: 'PUT',
+          // headers: {},
 
-      return $http(S3RequestOptions)
+          // The file's type is the key, and the value is the actual file to upload
+          data: files[filesWithPresignedURL.type]
+        };
+
+        return $http(S3RequestOptions);
+
+      });
+
+      return $q.all(promises)
         .then(function(response) {
           console.log('response from S3: ', response);
         })
