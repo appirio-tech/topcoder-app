53d1b4900d8151c3dd30af038c90193bdb4adf68
Nick Litwin <nlitwin@appirio.com>
2016-01-05 14:21:24 -0800

    Add validation and error handling for file inputs

--- app/directives/tc-file-input/tc-file-input.directive.js
+++ app/directives/tc-file-input/tc-file-input.directive.js
@@ -6,20 +6,25 @@
   function tcFileInput() {
     return {
       restrict: 'E',
-      require: '^form',
+      require: ['^form', '^ngModel'],
       templateUrl: 'directives/tc-file-input/tc-file-input.html',
       scope: {
         labelText: '@',
         fieldId: '@',
         placeholder: '@',
         fileType: '@',
+        showFileType: '=',
         mandatory: '=',
         buttonText: '@',
         setFileReference: '&',
-        fileValue: '=ngModel'
+        ngModel: '='
       },
-      link: function(scope, element, attrs, formController) {
+      link: function(scope, element, attrs, controllers) {
+        var formController = controllers[0];
+        var ngModel = controllers[1];
+
         scope.selectFile = selectFile;
+        var fileTypes = scope.fileType.split(',');
 
         // fieldId is not set on element at this point, so grabbing with class .none
         // which exists on the element right away
@@ -29,12 +34,28 @@
         fileInput.bind('change', function() {
           var file = fileInput[0].files[0];
 
-          // Pass file object up through callback into controller
-          scope.setFileReference({file: file, fieldId: scope.fieldId});
+          var selectedFileType = file.type.slice(file.type.lastIndexOf('/') + 1);
+          var isAllowedFileFormat = _.some(fileTypes, _.matches(selectedFileType));
 
-          // Set the file name as the value of the disabled input
-          fileNameInput[0].value = file.name;
-          formController[attrs.fieldId].$setValidity('required', true);
+          scope.$apply(function(){
+            if (!isAllowedFileFormat) {
+              fileNameInput[0].value = file.name;
+
+              // Manually setting is required since Angular doesn't support file inputs
+              formController[attrs.fieldId].$setTouched();
+              formController[attrs.fieldId].$setValidity('required', false);
+
+            } else {
+              // Pass file object up through callback into controller
+              scope.setFileReference({file: file, fieldId: scope.fieldId});
+
+              // Set the file name as the value of the disabled input
+              fileNameInput[0].value = file.name;
+
+              // Manually set validity of specific input field
+              formController[attrs.fieldId].$setValidity('required', true);
+            }
+          });
         });
 
         function selectFile() {
