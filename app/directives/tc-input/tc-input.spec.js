646c004ccff70144afe883b320c262078d49a669
Nick Litwin <nlitwin@appirio.com>
2016-01-14 10:24:25 -0800

    Add spec file

---
+++ app/directives/tc-input/tc-input.spec.js
@@ -0,0 +1,17 @@
+/* jshint -W117, -W030 */
+describe.only('Topcoder Input Directive', function() {
+  var scope;
+
+  beforeEach(function() {
+    bard.appModule('tcUIComponents');
+    bard.inject(this, '$compile', '$rootScope');
+  });
+
+  bard.verifyNoOutstandingHttpRequests();
+
+  describe('', function() {
+    beforeEach(function() {});
+
+    it('', function() {});
+  });
+});
