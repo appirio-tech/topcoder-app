1a83d652a0af587cd768a9355843fc5dd074814c
Nick Litwin <nlitwin@appirio.com>
2016-01-15 17:25:27 -0800

    Add spec file for tc-file-input

--- app/directives/tc-file-input/tc-file-input.spec.js
+++ app/directives/tc-file-input/tc-file-input.spec.js
@@ -1,19 +1,58 @@
 /* jshint -W117, -W030 */
-describe('Topcoder File Input Directive', function() {
-  var scope;
-
-  // USE AS TEMPLATE FOR DIRECTIVES
+describe.only('Topcoder File Input Directive', function() {
+  var scope, element, isolateScope;
 
   beforeEach(function() {
-    bard.appModule('tcUIComponents');
+    bard.appModule('topcoder');
     bard.inject(this, '$compile', '$rootScope');
+    scope = $rootScope.$new();
+
+    var html = '' +
+      '<form>' +
+        '<tc-file-input ' +
+          'label-text="Preview Image"' +
+          'field-id="DESIGN_COVER"' +
+          'button-text="Add File"' +
+          'file-type="jpg,jpeg,png"' +
+          'placeholder="Image file as .jpg or .png"' +
+          'mandatory="true"' +
+          'set-file-reference="vm.setFileReference(file, fieldId)"' +
+          'ng-model="vm.submissionForm.submissionZip"' +
+        ' />' +
+      '</form>';
+    var form = angular.element(html);
+    element = form.find('tc-file-input');
+    var formElement = $compile(form)(scope);
+    scope.$digest();
+
+    isolateScope = element.isolateScope();
   });
 
   bard.verifyNoOutstandingHttpRequests();
 
-  xdescribe('', function() {
-    beforeEach(function() {});
+  describe('selectFile', function() {
+    it('triggers a click on the file input', function() {
+      var fileInput = $(element).find('.none')[0];
+      var mockClick = sinon.spy(fileInput, 'click');
 
-    it('', function() {});
+      isolateScope.selectFile();
+      scope.$digest();
+
+      expect(mockClick).calledOnce;
+    });
+  });
+
+  describe('change events on the file input', function() {
+    it('do things', function() {
+      expect(false).to.be.true;
+    });
+
+    it('do other things', function() {
+      expect(false).to.be.true;
+    });
+
+    it('yet some more things', function() {
+      expect(false).to.be.true;
+    });
   });
 });
