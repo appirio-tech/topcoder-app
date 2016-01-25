fc10463145b5b507f903b15bdcae314cf273bfac
Nick Litwin <nlitwin@appirio.com>
2016-01-25 09:22:50 -0800

    Add starting webpack config

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+require('./node_modules/coffee-script/register');
+
+if (process.env.TRAVIS_BRANCH == 'master') process.env.ENV = 'PROD'
+if (process.env.TRAVIS_BRANCH == 'dev') process.env.ENV = 'DEV'
+if (process.env.TRAVIS_BRANCH == 'qa') process.env.ENV = 'QA'
+
+config = require('appirio-tech-webpack-config')({
+  dirname: __dirname,
+  entry: {
+    app: './app/index'
+  },
+  template: './app/index.html'
+});
+
+module.exports = config;
