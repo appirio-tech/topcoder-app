1a77630ad7c07fc7fe58c3681b28f92fe74f5c98
Nick Litwin <nlitwin@appirio.com>
2016-02-02 09:50:56 -0800

    Add back semicolons

--- app/submissions/submissions.routes.js
+++ app/submissions/submissions.routes.js
@@ -1,10 +1,10 @@
 (function() {
-  'use strict'
+  'use strict';
 
   angular.module('tc.submissions').config([
     '$stateProvider',
     routes
-  ])
+  ]);
 
   function routes($stateProvider) {
     var states = {
@@ -35,60 +35,60 @@
         controller: 'SubmitDevelopFilesController',
         controllerAs: 'vm'
       }
-    }
+    };
 
     function ChallengeToSubmitTo(ChallengeService, $stateParams, UserService) {
       // This page is only available to users that are registered to the challenge (submitter role) and the challenge is in the Checkpoint Submission or Submission phase.
       var params = {
         filter: 'id=' + $stateParams.challengeId
-      }
+      };
 
-      var userHandle = UserService.getUserIdentity().handle
+      var userHandle = UserService.getUserIdentity().handle;
 
-      var error = null
+      var error = null;
 
       return ChallengeService.getUserChallenges(userHandle, params)
         .then(function(challenge) {
           if (!challenge[0]) {
-            setErrorMessage('challenge', 'This is not a valid challenge. Use your browser\'s back button to return.')
+            setErrorMessage('challenge', 'This is not a valid challenge. Use your browser\'s back button to return.');
             return {
               error: error,
               challenge: null
-            }
+            };
           }
 
-          challenge = challenge[0].plain()
+          challenge = challenge[0].plain();
 
-          var phaseType
-          var phaseId
+          var phaseType;
+          var phaseId;
 
           var isPhaseSubmission = _.some(challenge.currentPhases, function(phase) {
             if (phase.phaseStatus === 'Open') {
               if (phase.phaseType === 'Submission') {
-                phaseType = 'SUBMISSION'
-                phaseId = phase.id
-                return true
+                phaseType = 'SUBMISSION';
+                phaseId = phase.id;
+                return true;
 
               } else if (phase.phaseType === 'Checkpoint Submission') {
-                phaseType = 'CHECKPOINT_SUBMISSION'
-                phaseId = phase.id
-                return true
+                phaseType = 'CHECKPOINT_SUBMISSION';
+                phaseId = phase.id;
+                return true;
               }
             }
 
-            return false
-          })
+            return false;
+          });
 
           if (!isPhaseSubmission) {
-            setErrorMessage('phase', 'Submission phases are not currently open for this challenge.')
+            setErrorMessage('phase', 'Submission phases are not currently open for this challenge.');
           }
 
           var isSubmitter = _.some(challenge.userDetails.roles, function(role) {
-            return role === 'Submitter'
-          })
+            return role === 'Submitter';
+          });
 
           if (!isSubmitter) {
-            setErrorMessage('submitter', 'You do not have a submitter role for this challenge.')
+            setErrorMessage('submitter', 'You do not have a submitter role for this challenge.');
           }
 
           return {
@@ -96,15 +96,15 @@
             challenge: challenge,
             phaseType: phaseType,
             phaseId: phaseId
-          }
+          };
         })
         .catch(function(err) {
-          setErrorMessage('challenge', 'There was an error getting information for this challenge.')
+          setErrorMessage('challenge', 'There was an error getting information for this challenge.');
 
           return {
             error: error,
             challenge: null
-          }
+          };
         })
 
       function setErrorMessage(type, message) {
@@ -113,14 +113,14 @@
           error = {
             type: type,
             message: message
-          }
+          };
         }
       }
     }
 
     for (var name in states) {
-      var state = states[name]
-      $stateProvider.state(name, state)
+      var state = states[name];
+      $stateProvider.state(name, state);
     }
   }
 })()
