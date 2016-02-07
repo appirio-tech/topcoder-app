2d753e5416055233b86275e0015d10a03989d58f
Colin Hunt <colin@arcsec.ca>
2016-02-07 10:52:29 +0700

    modified tabs to work with browser history navigation - Issue 673

--- app/directives/tc-tabs/tc-tabs.directive.js
+++ app/directives/tc-tabs/tc-tabs.directive.js
@@ -11,26 +11,49 @@ import angular from 'angular'
         bindToController: true,
         template: require('./tc-tabs')(),
         scope: {},
-        controller: ['$log', function($log, $scope, $element) {
+        controller: ['$log', '$location', '$scope', function($log, $location, $scope, $element) {
           $log = $log.getInstance('TcTabSetController')
           var self = this
           self.tabs = []
           self.addTab = function addTab(tab) {
             self.tabs.push(tab)
-            if (self.tabs.length === 1) {
-              tab.active = true
+            if (!angular.isDefined($location.search().tab) && self.tabs.length === 1) {
+              tab.active = true;
+            } else if ($location.search().tab === tab.heading) {
+              tab.active = true;
             }
           }
 
           self.select = function(selectedTab) {
+            var select = false;
             angular.forEach(self.tabs, function(tab) {
-              if (tab.active && tab !== selectedTab) {
-                tab.active = false
+              if (tab.active && tab.heading !== selectedTab.heading) {
+                tab.active = false;
+              } else if (tab.heading === selectedTab.heading) {
+                tab.active = true;
+                select = true;
               }
-            })
-
-            selectedTab.active = true
+            });
+            if (select === false && self.tabs.length > 0) {
+              self.tabs[0].active = true;
+            }
           }
+
+          self.setTab = function(tab) {
+            if ($location.search().tab !== tab.heading) {
+              $location.search('tab', tab.heading);
+            }
+          }
+          $scope.$on( "$locationChangeSuccess", function(){
+            if (angular.isDefined($location.search().tab)) {
+              var tab = $location.search().tab;
+              self.select({'heading' : tab});
+            } else if (self.tabs.length > 0) {
+              var tab = self.tabs[0].heading;
+              self.select({'heading' : tab});
+            }
+          });
+
         }],
         controllerAs: 'tabSet'
       }
