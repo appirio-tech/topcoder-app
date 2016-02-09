84a15a9d08619879d34bc48fafdf30f24fbc05e4
vikasrohit <vikas.agarwal@appirio.com>
2016-02-09 16:10:16 +0530

    Revert "SUP-3055, [Edit Profile] Can't delete weblink in profile"
    This reverts commit b77587e6f671d542ce688d96269a1d9d08121e46.

--- app/services/api.service.js
+++ app/services/api.service.js
@@ -91,7 +91,6 @@ import _ from 'lodash'
             id: 'handle',
             route: 'members'
           })
-          .setDefaultHeaders({ "Content-Type": "application/json" })
           .addRequestInterceptor(function(element, operation, what, url) {
             if (url.indexOf('members') > -1 || (operation.toLowerCase() === 'post' && url.indexOf('profiles') > -1)) {
               return {
