3e9b33fca22ffc2956797adcffbca23117f57346
Nick Litwin <nlitwin@appirio.com>
2016-02-10 13:26:34 -0800

    Fix merge conflicts

--- app/peer-review/completed-review/completed-review.controller.js
+++ app/peer-review/completed-review/completed-review.controller.js
@@ -1,64 +1,65 @@
+import angular from 'angular'
+
 (function() {
-  'use strict';
+  'use strict'
 
-  angular.module('tc.peer-review').controller('CompletedReviewController',  CompletedReviewController);
+  angular.module('tc.peer-review').controller('CompletedReviewController',  CompletedReviewController)
 
-  CompletedReviewController.$inject = ['$stateParams', 'ScorecardService', 'ReviewService', 'UserService', 'ChallengeService', 'Helpers', '$q', 'CONSTANTS'];
+  CompletedReviewController.$inject = ['$stateParams', 'ScorecardService', 'ReviewService', 'UserService', 'ChallengeService', 'Helpers', '$q', 'CONSTANTS', '$state']
 
-  function CompletedReviewController($stateParams, ScorecardService, ReviewService, UserService, ChallengeService, Helpers, $q, CONSTANTS) {
-    var vm = this;
-    vm.domain = CONSTANTS.domain;
-    vm.challengeId = $stateParams.challengeId;
-    vm.loaded = false;
-    vm.stats = {};
+  function CompletedReviewController($stateParams, ScorecardService, ReviewService, UserService, ChallengeService, Helpers, $q, CONSTANTS, $state) {
+    var vm = this
+    vm.domain = CONSTANTS.domain
+    vm.challengeId = $stateParams.challengeId
+    vm.loaded = false
+    vm.stats = {}
     vm.scorecard = {
       questions: {}
-    };
-    var handle = UserService.getUserIdentity().handle;
+    }
+    var handle = UserService.getUserIdentity().handle
     vm.submit = function() {
       $state.go('review.status', {
         challengeId: vm.challengeId
-      });
-    };
+      })
+    }
 
-    activate();
+    activate()
 
     function activate() {
       var promises = [
         ChallengeService.getChallengeDetails($stateParams.challengeId),
         ReviewService.getReview($stateParams.reviewId),
         ScorecardService.getScorecard(vm.challengeId)
-      ];
+      ]
 
       $q.all(promises)
       .then(function(responses) {
-        vm.stats.username = handle;
+        vm.stats.username = handle
 
-        vm.challenge = responses[0];
+        vm.challenge = responses[0]
 
-        vm.review = responses[1];
-        vm.stats.submissionId = vm.review.submissionId;
-        vm.stats.uploadId = vm.review.uploadId;
-        vm.stats.createdAt = vm.review.createdAt;
-        vm.stats.updatedAt = vm.review.updatedAt;
+        vm.review = responses[1]
+        vm.stats.submissionId = vm.review.submissionId
+        vm.stats.uploadId = vm.review.uploadId
+        vm.stats.createdAt = vm.review.createdAt
+        vm.stats.updatedAt = vm.review.updatedAt
 
-        var scorecardData = responses[2][0];
-        var scorecardId = scorecardData.id;
+        var scorecardData = responses[2][0]
+        var scorecardId = scorecardData.id
 
         ScorecardService.getScorecardQuestions(scorecardId)
         .then(function(questions) {
-          Helpers.storeById(vm.scorecard.questions, questions);
-          Helpers.parseQuestions(vm.scorecard.questions);
+          Helpers.storeById(vm.scorecard.questions, questions)
+          Helpers.parseQuestions(vm.scorecard.questions)
 
-          return ReviewService.getReviewItems($stateParams.reviewId);
+          return ReviewService.getReviewItems($stateParams.reviewId)
 
         }).then(function(answers) {
+          Helpers.parseAnswers(vm.scorecard.questions, answers)
 
-          Helpers.parseAnswers(vm.scorecard.questions, answers);
-
-          vm.loaded = true;
-        });
-      });
+          vm.loaded = true
+        })
+      })
     }
-  };
-})();
+  }
+})()
--- app/peer-review/edit-review/edit-review.controller.js
+++ app/peer-review/edit-review/edit-review.controller.js
@@ -1,103 +1,105 @@
+import angular from 'angular'
+
 (function() {
-  'use strict';
+  'use strict'
 
   // The EditReviewController controller fetches the scorecard template and any
   // answers saved but not submitted.
-  angular.module('tc.peer-review').controller('EditReviewController', EditReviewController);
+  angular.module('tc.peer-review').controller('EditReviewController', EditReviewController)
 
-  EditReviewController.$inject = ['$state', '$stateParams', 'ReviewService', 'ScorecardService', 'UserService', 'ChallengeService', 'Helpers', '$q', 'CONSTANTS'];
+  EditReviewController.$inject = ['$state', '$stateParams', 'ReviewService', 'ScorecardService', 'UserService', 'ChallengeService', 'Helpers', '$q', 'CONSTANTS']
 
   function EditReviewController($state, $stateParams, ReviewService, ScorecardService, UserService, ChallengeService, Helpers, $q, CONSTANTS) {
-    var vm = this;
-    vm.domain = CONSTANTS.domain;
-    vm.challengeId = $stateParams.challengeId;
-    vm.challenge = null;
-    vm.loaded = false;
-    vm.saved = false;
-    vm.stats = {};
+    var vm = this
+    vm.domain = CONSTANTS.domain
+    vm.challengeId = $stateParams.challengeId
+    vm.challenge = null
+    vm.loaded = false
+    vm.saved = false
+    vm.stats = {}
     vm.scorecard = {
       questions: {}
-    };
-    var handle = UserService.getUserIdentity().handle;
+    }
+    var handle = UserService.getUserIdentity().handle
 
     vm.submitReviewItems = function() {
-      var body = Helpers.compileReviewItems(vm.scorecard.questions, vm.review, vm.saved);
+      var body = Helpers.compileReviewItems(vm.scorecard.questions, vm.review, vm.saved)
 
       ReviewService.saveReviewItems(body, vm.saved)
       .then(function(response) {
-        return ReviewService.markAsCompleted($stateParams.reviewId);
+        return ReviewService.markAsCompleted($stateParams.reviewId)
       })
       .then(function(response) {
         $state.go('review.status', {
           challengeId: vm.challengeId
-        });
+        })
       })
       .catch(function(error) {
-        var message = 'An error occurred while trying to submit answers.\n' + error.status + ': ' + error.statusText;
+        var message = 'An error occurred while trying to submit answers.\n' + error.status + ': ' + error.statusText
 
-        alert(message);
+        alert(message)
 
-        $state.reload();
-      });
-    };
+        $state.reload()
+      })
+    }
 
     vm.saveReviewForLater = function() {
-      var body = Helpers.compileReviewItems(vm.scorecard.questions, vm.review, vm.saved);
+      var body = Helpers.compileReviewItems(vm.scorecard.questions, vm.review, vm.saved)
 
       ReviewService.saveReviewItems(body, vm.saved)
       .then(function(data) {
         $state.go('review.status', {
           challengeId: vm.challengeId
-        });
+        })
       })
       .catch(function(error) {
-        var message = 'An error occurred while trying to update answers.\n' + error.status + ': ' + error.statusText;
+        var message = 'An error occurred while trying to update answers.\n' + error.status + ': ' + error.statusText
 
-        alert(message);
+        alert(message)
 
-        $state.reload();
-      });
-    };
+        $state.reload()
+      })
+    }
 
-    activate();
+    activate()
 
     function activate() {
       var promises = [
         ChallengeService.getChallengeDetails(vm.challengeId),
         ReviewService.getReview($stateParams.reviewId),
         ScorecardService.getScorecard(vm.challengeId)
-      ];
+      ]
 
       $q.all(promises)
       .then(function(responses) {
-        vm.stats.username = handle;
+        vm.stats.username = handle
 
-        vm.challenge = responses[0];
+        vm.challenge = responses[0]
 
-        vm.review = responses[1];
-        vm.stats.submissionId = vm.review.submissionId;
-        vm.stats.uploadId = vm.review.uploadId;
-        vm.stats.createdAt = vm.review.createdAt;
-        vm.stats.updatedAt = vm.review.updatedAt;
+        vm.review = responses[1]
+        vm.stats.submissionId = vm.review.submissionId
+        vm.stats.uploadId = vm.review.uploadId
+        vm.stats.createdAt = vm.review.createdAt
+        vm.stats.updatedAt = vm.review.updatedAt
 
-        var scorecardData = responses[2][0];
-        var scorecardId = scorecardData.id;
+        var scorecardData = responses[2][0]
+        var scorecardId = scorecardData.id
 
         ScorecardService.getScorecardQuestions(scorecardId)
         .then(function(questions) {
 
-          Helpers.storeById(vm.scorecard.questions, questions);
-          Helpers.parseQuestions(vm.scorecard.questions);
+          Helpers.storeById(vm.scorecard.questions, questions)
+          Helpers.parseQuestions(vm.scorecard.questions)
 
-          return ReviewService.getReviewItems($stateParams.reviewId);
+          return ReviewService.getReviewItems($stateParams.reviewId)
 
         })
         .then(function(answers) {
-          vm.saved = Helpers.parseAnswers(vm.scorecard.questions, answers);
+          vm.saved = Helpers.parseAnswers(vm.scorecard.questions, answers)
 
-          vm.loaded = true;
-        });
-      });
+          vm.loaded = true
+        })
+      })
     }
-  };
-})();
+  }
+})()
