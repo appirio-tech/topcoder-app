215c1eab05ad092db03a414c1dddf4ab1646a1ea
vikasrohit <vikas.agarwal@appirio.com>
2016-03-21 12:10:00 +0530

    AS#101110714058011, User cannot add custom URL to profile
    -- Increased supported length to 15 from 3 for TLD

--- app/directives/external-account/external-web-links.directive.js
+++ app/directives/external-account/external-web-links.directive.js
@@ -25,7 +25,7 @@ import angular from 'angular'
       $log = $log.getInstance('ExternalWebLinkCtrl')
       $scope.addingWebLink = false
       $scope.errorMessage = null
-      $scope.urlRegEx = /^(http(s?):\/\/)?(www\.)?[a-zA-Z0-9\.\-\_]+(\.[a-zA-Z]{2,3})+(\/[a-zA-Z0-9\_\-\s\.\/\?\%\#\&\=]*)?$/
+      $scope.urlRegEx = /^(http(s?):\/\/)?(www\.)?[a-zA-Z0-9\.\-\_]+(\.[a-zA-Z]{2,15})+(\/[a-zA-Z0-9\_\-\s\.\/\?\%\#\&\=]*)?$/
 
       $scope.addWebLink = function() {
         $log.debug('URL: ' + $scope.url)
