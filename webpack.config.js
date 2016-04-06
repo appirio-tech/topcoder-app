35bdf696016997be572a509ff55bc96d8a968346
Nick Litwin <nlitwin@appirio.com>
2016-04-06 16:55:21 -0700

    Remove console logs

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,10 @@
-/*eslint-disable*/
 require('./node_modules/coffee-script/register')
 
 const CI = process.env.TRAVIS_BRANCH
 
-console.log('before **************', process.env.ENV)
 if (CI == 'master')         process.env.ENV = 'PROD'
 if (CI == 'qa-integration') process.env.ENV = 'QA'
 if (CI == 'dev')            process.env.ENV = 'DEV'
-
-console.log('***************', CI)
-console.log('***************', process.env.ENV)
 
 const config = require('appirio-tech-webpack-config')({
   dirname: __dirname,
