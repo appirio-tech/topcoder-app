bbe134585d24f421f637375cd7d59a634bd12d8c
vikasrohit <vikas.agarwal@appirio.com>
2016-04-25 16:04:03 +0530

    AS#100297043256582, Move all login to link of Members to accounts.topcoder.com
    -- Reverted changes for window.location, because it caused infinite redirect

--- app/topcoder.module.js
+++ app/topcoder.module.js
@@ -55,8 +55,8 @@ import { getCurrentUser, loadUser } from './services/userv3.service.js'
           logger.debug('successful login with token ' + JSON.stringify(token))
           $rootScope.$broadcast(CONSTANTS.EVENT_USER_LOGGED_IN)
           logger.debug('Going to state: ' + toState.name)
-          // $state.go(toState.name, toParams, {notify: false})
-          $window.location = $state.href(toState.name, toParams, {absolute: true})
+          $state.go(toState.name, toParams, {notify: false})
+          $rootScope.$apply()
         }, function() {
           logger.debug('State requires authentication, and user is not logged in, redirecting')
           // setup redirect for post login
