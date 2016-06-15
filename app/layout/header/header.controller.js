c1bc06b0e24ebd587707d423ec0aca97f99dee2d
vikasrohit <vikas.agarwal@appirio.com>
2016-06-15 12:52:41 +0530

    AS#100297043256582, Move all login to link of Members to accounts.topcoder.com
    -- Fixed trailing slash problem with login route
    -- Fixed infinite loop issue with member profile page's public access

--- app/layout/header/header.controller.js
+++ app/layout/header/header.controller.js
@@ -1,14 +1,15 @@
 import angular from 'angular'
 import _ from 'lodash'
+import { getCurrentUser, loadUser } from '../../services/userv3.service.js'
 
 (function() {
   'use strict'
 
   angular.module('tc.layout').controller('HeaderController', HeaderController)
 
-  HeaderController.$inject = ['$state', 'TcAuthService', 'CONSTANTS', 'logger', '$rootScope', 'UserService', 'ProfileService', 'NavService']
+  HeaderController.$inject = ['$scope', '$state', 'TcAuthService', 'CONSTANTS', 'logger', '$rootScope', 'UserService', 'ProfileService', 'NavService']
 
-  function HeaderController($state, TcAuthService, CONSTANTS, logger, $rootScope, UserService, ProfileService, NavService) {
+  function HeaderController($scope, $state, TcAuthService, CONSTANTS, logger, $rootScope, UserService, ProfileService, NavService) {
     var vm = this
 
     vm.constants = CONSTANTS
@@ -31,7 +32,7 @@ import _ from 'lodash'
     activate()
 
     function activate() {
-      initHeaderProps('default')
+      validateAndInitHeader('default')
 
       // List of events that might force header update
       angular.forEach([
@@ -40,35 +41,52 @@ import _ from 'lodash'
         CONSTANTS.EVENT_PROFILE_UPDATED
       ], function(event) {
         $rootScope.$on(event, function() {
-          initHeaderProps(event)
+          validateAndInitHeader(event)
         })
       })
+    }
+
+    /**
+     * Validates login state and then initiate header update. If user is not loaded yet, i.e.,
+     * page is reloaded or page is loaded for the first time, currentUser is not set yet. In this
+     * case we try to load user first and if we are able to load user, update header properties
+     * otherwise it assumes the page to be loaded without login.
+     */
+    function validateAndInitHeader(event) {
+      vm.isAuth = TcAuthService.isAuthenticated()
+      if (vm.isAuth) {
+        initHeaderProps(event)
+      } else {
+        loadUser().then(function(token) {
+          // update auth flag
+          vm.isAuth = TcAuthService.isAuthenticated()
+          initHeaderProps(event)
+        }, function(error) {
+          // do nothing, just show non logged in state of navigation bar
+        })
+      }
     }
 
     function initHeaderProps(event) {
       logger.debug(event + ' triggered header update.')
 
-      vm.isAuth = TcAuthService.isAuthenticated()
+      vm.userHandle = UserService.getUserIdentity().handle
 
-      if (vm.isAuth) {
-        vm.userHandle = UserService.getUserIdentity().handle
+      vm.userMenu = [
+        { 'sref': 'dashboard', 'text': 'DASHBOARD', 'icon': require('../../../assets/images/nav/dashboard.svg') },
+        { 'sref': 'profile.about', 'srefParams': { 'userHandle': vm.userHandle }, 'text': 'MY PROFILE', 'icon': require('../../../assets/images/nav/profile.svg') },
+        { 'href':  vm.constants.COMMUNITY_URL + '/PactsMemberServlet?module=PaymentHistory&full_list=false', 'text': 'PAYMENTS', 'icon': require('../../../assets/images/nav/wallet.svg') },
+        { 'sref': 'settings.profile', 'text': 'SETTINGS', 'icon': require('../../../assets/images/nav/settings.svg') }
+      ]
 
-        vm.userMenu = [
-          { 'sref': 'dashboard', 'text': 'DASHBOARD', 'icon': require('../../../assets/images/nav/dashboard.svg') },
-          { 'sref': 'profile.about', 'srefParams': { 'userHandle': vm.userHandle }, 'text': 'MY PROFILE', 'icon': require('../../../assets/images/nav/profile.svg') },
-          { 'href':  vm.constants.COMMUNITY_URL + '/PactsMemberServlet?module=PaymentHistory&full_list=false', 'text': 'PAYMENTS', 'icon': require('../../../assets/images/nav/wallet.svg') },
-          { 'sref': 'settings.profile', 'text': 'SETTINGS', 'icon': require('../../../assets/images/nav/settings.svg') }
-        ]
-
-        ProfileService.getUserProfile(vm.userHandle)
-        .then(function(data) {
-          vm.profile = data
-          vm.userHandleColor = ProfileService.getUserHandleColor(vm.profile)
-        })
-        .catch(function(err) {
-          logger.error('Unable to get user profile data', err)
-        })
-      }
+      return ProfileService.getUserProfile(vm.userHandle)
+      .then(function(data) {
+        vm.profile = data
+        vm.userHandleColor = ProfileService.getUserHandleColor(vm.profile)
+      })
+      .catch(function(err) {
+        logger.error('Unable to get user profile data', err)
+      })
     }
 
     function selectedGroup() {
