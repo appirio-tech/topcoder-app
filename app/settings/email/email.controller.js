f5be828f76d7725d8ca8f20f68cecca5fd36c41c
Vikas Agarwal <vikas.agarwal@topcoder.com>
2016-10-27 12:25:41 +0530

    Asana task#202430448250910 Predix - Add option to subscribe to Predix member settings page (like we did with iOS) -- Front end changes

--- app/settings/email/email.controller.js
+++ app/settings/email/email.controller.js
@@ -59,6 +59,13 @@ import angular from 'angular'
           desc: 'Our annual online and onsite tournament to celebrate and reward the community',
           enabled: false,
           dirty: false
+        },
+        {
+          id: 'TOPCODER_NL_PREDIX',
+          name: 'Predix Community Newsletter',
+          desc: 'Our annual online and onsite tournament to celebrate and reward the community',
+          enabled: false,
+          dirty: false
         }
       ]
 
