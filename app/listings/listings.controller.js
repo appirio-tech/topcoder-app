8090e66649ecc41bd66da3a143c8963538cc2266
birdofpreyru <birdofpreyru@examples.com>
2017-01-25 10:34:01 -0500

    filters fixes
    Signed-off-by: Tom Ladendorf <tom@tomladendorf.com>

--- app/listings/listings.controller.js
+++ app/listings/listings.controller.js
@@ -5,14 +5,21 @@ import angular from 'angular'
 
   angular.module('tc.listings').controller('ListingsCtrl', ListingsCtrl)
 
-  ListingsCtrl.$inject = ['CONSTANTS', 'logger', '$q',
+  ListingsCtrl.$inject = ['$location', '$scope', 'CONSTANTS', 'logger', '$q',
     'TcAuthService', 'UserService', 'UserStatsService', 'ProfileService', 'ChallengeService', 'ExternalAccountService',
     'ngDialog', '$anchorScroll'
   ]
 
-  function ListingsCtrl(CONSTANTS, logger, $q, TcAuthService, UserService, UserStatsService, ProfileService, ChallengeService, ExternalAccountService, ngDialog, $anchorScroll) {
+  function ListingsCtrl($location, $scope, CONSTANTS, logger, $q, TcAuthService, UserService, UserStatsService, ProfileService, ChallengeService, ExternalAccountService, ngDialog, $anchorScroll) {
 
     activate()
+
+    $scope.listingProps = {
+      filterFromUrl: $location.hash(),
+      onSaveFilterToUrl: function(filter) {
+        $location.hash(filter)
+      }
+    }
 
     function activate() {
       logger.debug('Calling ListingsController activate()')
