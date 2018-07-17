d06469d1912d7b85b3b6d165137e9cb24bd90445
nkumar-topcoder <33625707+nkumar-topcoder@users.noreply.github.com>
2018-07-17 17:26:39 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -20,8 +20,8 @@ module.exports = function(config) {
 
     // List of files / patterns to load in the browser
     files: [
-      './node_modules/jquery/dist/jquery.js',
       './node_modules/babel-polyfill/dist/polyfill.js',
+      './node_modules/jquery/dist/jquery.js',
       'webpack.tests.js'
     ],
 
